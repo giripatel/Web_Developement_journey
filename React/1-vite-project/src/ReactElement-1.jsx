841ad0d420b578be057385457246d9395a218169
Giridhar <giridharpatel143@gmail.com>
2023-11-04 19:26:29 +0530

    React hooks basics

---
+++ React/1-vite-project/src/ReactElement-1.jsx
@@ -0,0 +1,6 @@
+const Test = ()=>{
+    return (
+        <h1>Hello world</h1>
+    )
+}
+export default Test;
---
+++ React/2-vite-project/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  // const [count,setCount] = 0;
+
+  const increaseCount = ()=>{
+    if(count < 20){
+    setCount(count+1);
+    }
+  }
+  const decreaseCount = () => {
+    if(count > 0){
+      setCount(count-1);
+      }
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <h2>{count}</h2>
+      <div className="card">
+        <button onClick={increaseCount}> Increase 
+        </button>
+        <h1>count is {count}</h1>
+        <button onClick={decreaseCount}>
+          Decrease
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <h3>{count}</h3>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
