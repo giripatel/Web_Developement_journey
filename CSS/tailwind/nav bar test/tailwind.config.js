7d55b8ee1460b5b733e3558681d3915c5c0465a7
giripatel <giridharpatel143@gmail.com>
2024-02-25 14:06:04 +0530

    Create background blur navbar
    created a nav bar which has backdrop blur property and learned few blur usecases

---
+++ CSS/tailwind/nav bar test/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // backdropBlur: {
+    //   sm:'30px'
+    // }
+  },
+  plugins: [],
+}
